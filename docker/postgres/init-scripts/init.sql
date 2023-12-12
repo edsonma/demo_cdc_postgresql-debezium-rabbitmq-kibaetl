2209717fa571119aad7c5aabebad0a5b18805cd0
edsonma@gmail.com <edsonma@gmail.com>
2023-12-12 00:12:39 -0300

    chore: add some data to test table

--- docker/postgres/init-scripts/init.sql
+++ docker/postgres/init-scripts/init.sql
@@ -14,3 +14,9 @@ ALTER ROLE "pguser" WITH REPLICATION LOGIN;
 GRANT SELECT ON TABLE test TO "pguser";
 
 CREATE PUBLICATION publication_role FOR ALL TABLES;
+
+INSERT INTO test (name, age, city) VALUES ('Mike Tyson', 57, 'Las Vegas');
+INSERT INTO test (name, age, city) VALUES ('Floyd Mayweather Jr', 46, 'Las Vegas');
+INSERT INTO test (name, age, city) VALUES ('Canelo Alvarez', 33, 'Jalisco');
+INSERT INTO test (name, age, city) VALUES ('Ryan Garcia', 25, 'Los Angeles');
+
