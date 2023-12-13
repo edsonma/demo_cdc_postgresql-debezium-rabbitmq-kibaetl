3e51625924cc3ce9ca234d2c73ab7121a70d7b1b
edsonma@gmail.com <edsonma@gmail.com>
2023-12-13 11:39:48 -0300

    chore: more setup

--- docker/postgres/init-scripts/init.sql
+++ docker/postgres/init-scripts/init.sql
@@ -9,11 +9,19 @@ CREATE TABLE IF NOT EXISTS test (
 );
 
 ALTER TABLE test REPLICA IDENTITY FULL;
+CREATE PUBLICATION publication_role FOR ALL TABLES;
 
 ALTER ROLE "pguser" WITH REPLICATION LOGIN;
-GRANT SELECT ON TABLE test TO "pguser";
 
-CREATE PUBLICATION publication_role FOR ALL TABLES;
+GRANT SELECT ON TABLE test TO "pguser";
+GRANT CONNECT ON DATABASE "source_db" TO "pguser";
+
+GRANT USAGE ON SCHEMA "public" TO  "pguser";
+GRANT TEMPORARY ON DATABASE "source_db" TO "pguser";
+GRANT USAGE ON SCHEMA pg_catalog TO pguser;
+
+GRANT pg_read_all_settings TO pguser;
+GRANT pg_read_all_stats TO pguser;
 
 INSERT INTO test (name, age, city) VALUES ('Mike Tyson', 57, 'Las Vegas');
 INSERT INTO test (name, age, city) VALUES ('Floyd Mayweather Jr', 46, 'Las Vegas');
